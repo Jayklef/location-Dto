840b2ab7ace115c561fa4f309070643acd0913f9
Jayklef <jerryarhawho@gmail.com>
2023-10-24 13:25:03 +0100

    initial setup

---
+++ src/main/java/com/jayklef/locationdto/LocationDtoApplication.java
@@ -0,0 +1,13 @@
+package com.jayklef.locationdto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationDtoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocationDtoApplication.class, args);
+    }
+
+}
