87d841cdcf3cb307a9b27fdf7847e5f3f064398a
Jayklef <jerryarhawho@gmail.com>
2023-10-24 13:35:59 +0100

    User and Location Model

---
+++ src/main/java/com/jayklef/locationdto/model/Location.java
@@ -0,0 +1,21 @@
+package com.jayklef.locationdto.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "locations")
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String place;
+    private Double latitude;
+    private Double longitude;
+}
---
+++ src/main/java/com/jayklef/locationdto/model/User.java
@@ -0,0 +1,22 @@
+package com.jayklef.locationdto.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
